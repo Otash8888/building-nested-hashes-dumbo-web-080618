d27b5b966ab66c8c7d868d33ba5c5126b9b89154
Otabek Kamalov <otakam86@gmail.com>
2018-07-22 21:14:39 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,11 +2,17 @@ def first_challenge
    epic_tragedy = {
                   
                   montague:{
-                            patriarch
-                            matriarch
-                            hero
+                            patriarch:{}
+                            matriarch:{}
+                            hero:{}
+                            hero_friends:[]
                             },
-                  capulet:{}
+                  capulet:{
+                            patriarch:{}
+                            matriarch:{}
+                            hero:{}
+                            hero_friends:[]
+                            },
                   
           
    }
