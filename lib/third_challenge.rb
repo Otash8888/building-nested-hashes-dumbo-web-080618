a736eeb81002b38e1b69c18211c59ab08c3a4153
Otabek Kamalov <otakam86@gmail.com>
2018-07-22 21:22:09 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
         :age  => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name of "Lady Montague"
         },
       :hero => {
         #your key/value pairs here
